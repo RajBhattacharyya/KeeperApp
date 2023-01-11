3a8b0a84c6662e1c6a54b67233a00e5500fee75a
Raj Bhattacharyya <rajbhattacharyya18110@gmail.com>
2023-01-12 00:41:57 +0530

    minor changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,19 @@ import React from "react";
 import Header from "./Header.jsx";
 import Note from "./Note"
 import Footer from "./Footer.jsx";
+import notes from "../notes"
 
 export default function App() {
     return (
         <div>
             <Header />
-            <Note />
+            {notes.map(noteItem => (
+                <Note 
+                    key= {noteItem.key}
+                    title= {noteItem.title}
+                    content= {noteItem.content}
+                />
+            ))};
             <Footer />
         </div>  
     )
